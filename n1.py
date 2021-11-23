7d2bfa58708422abb2c2180ecdee7efb3240598d
Servelatus <megeve@yandex.ru>
2021-11-23 22:27:40 +0300

    Домашка №7

--- n1.py
+++ n1.py
@@ -1,45 +1,48 @@
-# 1. Реализовать класс «Дата»,
-# функция-конструктор которого должна принимать дату в виде строки формата «день-месяц-год».
-# В рамках класса реализовать два метода.
-# Первый, с декоратором @classmethod, должен извлекать число, месяц, год и преобразовывать их тип к типу «Число».
-# Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца и года (например, месяц — от 1 до 12).
-# Проверить работу полученной структуры на реальных данных.
+# 1. Реализовать класс Matrix (матрица). Обеспечить перегрузку конструктора класса (метод __init__()),
+# который должен принимать данные (список списков) для формирования матрицы.
+# Подсказка: матрица — система некоторых математических величин, расположенных в виде прямоугольной схемы.
+# Примеры матриц вы найдете в методичке.
+# Следующий шаг — реализовать перегрузку метода __str__() для вывода матрицы в привычном виде.
+# Далее реализовать перегрузку метода __add__() для реализации операции сложения двух объектов класса Matrix (двух матриц).
+# Результатом сложения должна быть новая матрица.
+# Подсказка: сложение элементов матриц выполнять поэлементно — первый элемент первой строки
+# первой матрицы складываем с первым элементом первой строки второй матрицы и т.д.
 
-class Data:
-    data_list = []
-    def __init__(self, data_str =  str):
+class Matrix:
+    def __init__(self, a, b, x =  list()):
 
-        Data.data_list = data_str.split('-')
+        self.x = x
+        self.a = a
+        self.b = b
 
-    @classmethod
-    def data_to_int(cls):
-        data_numb = int(''.join(cls.data_list))
-        print(data_numb)
+    def __str__(self):
+        for xx in self.x:
+            for yy in xx:
+                print (f'{yy:02}', end = ' ')
+            print('\n')
 
-    @staticmethod
-    def validation(data_str):
-        month_30 = {4, 6, 9, 11}
-        data_list = data_str.split('-')
-        # Не хватило времени на проверку високосного года!
-        data_list = [int(i) for i in data_list]
-
-        if 1 <= data_list[1] <= 12 and 1 <= data_list[0] <= 31:
-            if  30 < data_list[0]  and data_list[1] in month_30:
-                return False
-            else:
-                if 28 < data_list[0]  and data_list[1] == 2:
-                    return False
-                else:
-                     return True
-
-        else:
-            return False
+        return  ''
 
 
-dat = input('Введите дату: ')
-vremechko = Data(dat)
-if Data.validation(dat):
-    vremechko.data_to_int()
-else:
-    print('Дата введена неправильная какая-то((')
+    def __add__(self, other):
+        sum_matrix = []
+        for i in range(self.a):
+            sum_matrix.append([])
+            for k in range(self.b):
+                sum_matrix[i].append(0)
+
+        for row in range(self.a):
+            for el in range(self.b):
+               sum_matrix[row][el] = self.x[row][el] + other.x[row][el]
+        return Matrix(self.a, self.b, sum_matrix)
+
+
+
+matrix1 = Matrix(3, 4, [[1, 2, 3, 4],[11, 12, 13, 14], [21, 22, 23, 24]])
+matrix2 = Matrix(3, 4, [[1, 1, 1, 1],[1, 1, 1, 1], [1, 1, 1, 1]])
+
+print(matrix1, matrix2)
+super_matrix = matrix1 + matrix2
+print(super_matrix)
+
 
